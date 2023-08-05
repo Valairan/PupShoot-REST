36aa0ad25a231c88123d8be32f2cbabc2449f2bc
Tayfun Turgut <tayfun@modiface.com>
2023-08-05 14:47:10 -0400

    ps: Delete old files

--- app.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var mongo = require('mongodb')
-var app = express();
-
-app.get('/', function(req, res){
-    res.send("Hello World")
-});
-
-app.listen( process.env.port)
